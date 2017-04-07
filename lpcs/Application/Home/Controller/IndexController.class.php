5ca566bc7142885a3c7dbf2d1a0c1605dee6525f
pjw <980682473@qq.com>
2017-04-07 09:35:12 +0800

    修改

--- lpcs/Application/Home/Controller/IndexController.class.php
+++ lpcs/Application/Home/Controller/IndexController.class.php
@@ -8,6 +8,7 @@ class IndexController extends Controller
     public function index()
     {
         $callback = $_GET['callback'];
+        $user=D('user');
         $code = $_GET['code'];
         $AppId = 'wxbb97c4417d90216b';
         $AppSecret = '549a3b5ba125896f940ad0e236a9b52d';
@@ -15,18 +16,28 @@ class IndexController extends Controller
         $res = $this->getJson($url);
         $access_token = $res["access_token"];
         $openid = $res['openid'];
-        $url = "https://api.weixin.qq.com/sns/userinfo?access_token=" . $access_token . "&openid=" . $openid . "&lang=zh_CN";
-        $info = $this->getJson($url);
-        $arr = array(
-            "code" => "000",
-            "msg" => "",
-            "data" => array("openid" => $openid,
-                "nickname" => $info->nickname,
-                "headimg" => $info->headimgurl,
-                "sex" => $info->sex
-            )
-        );
-        echo $callback . "(" . HHJson($arr) . ")";
+        $userinfo=$user->where("openid='%s'",$openid)->find();
+        if($userinfo){
+            $arr = array(
+                "code" => "000",
+                "msg" => "",
+                "data" => $userinfo
+            );
+            echo $callback . "(" . HHJson($arr) . ")";
+        }else{
+            $url = "https://api.weixin.qq.com/sns/userinfo?access_token=" . $access_token . "&openid=" . $openid . "&lang=zh_CN";
+            $info = $this->getJson($url);
+            $data['nickname']=$info['nickname'];
+            $data['headimg']=$info['headimgurl'];
+            $data['openid']=$info['openid'];
+            $user->add($data);
+            $arr = array(
+                "code" => "000",
+                "msg" => "",
+                "data" => $info
+            );
+            echo $callback . "(" . HHJson($arr) . ")";
+        }
     }
     //$url  接口url string
     //$type 请求类型string
